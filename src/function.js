5d884eb264562a338b3c5b829c3d332aaf943b0e
JeffersonCarlosLima <jeffersoncl200@gmail.comgit config --global user.name JeffersonCarlosLimagit config --global user.email jeffersoncl200@gmail.com>
2023-08-07 13:40:22 -0300

    commit

--- src/function.js
+++ src/function.js
@@ -1,15 +1,21 @@
 function gerarLink() {
     var telefone = document.getElementById("telefone").value;
     var assunto = document.getElementById("texto").value;
-    console.log("Seu E-mail: " + telefone + "Assunto: " + assunto);
 
-    assunto = assunto.replace(" ", "%20");
+
+    console.log( telefone + assunto);
+
+    //assunto = assunto.replace(" ", "%20");
 
     //location.href = 'https://wa.me/' + telefone;
     //window.location.href = "https://wa.me/55" + telefone + "?text=" + assunto;
     link = "https://wa.me/55" + telefone + "?text=" + assunto;
-    navigator.clipboard.writeText(link);
-    
+    let encodeLink = encodeURI(link);
+    linkgerado = document.getElementById("linkwhatsapp").innerHTML = encodeLink;
 }
 
 //  https://wa.me/552196312XXXX ?text=Eu%20tenho%20interesse%20no%20seu%20carro%20à%20venda
+function copiar(){
+    navigator.clipboard.writeText(linkgerado);
+    console.log("dados copiados")
+    }
